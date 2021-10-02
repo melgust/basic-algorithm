9e0474bb5363d15a8112465b70261177a01e1b85
Melvin Cali <melgust@gmail.com>
2021-10-02 08:58:06 -0600

    Update

--- archivo.py
+++ archivo.py
@@ -7,6 +7,11 @@ if (n % 2 == 0) :
 else:
     print(n, 'no es par')
 
+path_new_file = 'out.txt'
+fileout = open(path_new_file, 'w')
+name = input('Escribe tu nombre: ')
+fileout.write(name)
+fileout.close()
 #'r' : use for reading
 #'w' : use for writing
 #'x' : use for creating and writing to a new file
